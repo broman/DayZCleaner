a6fd4456d38fa2dd63441d2e06eb5322bbb5f485
ryan broman <ryanjbroman@gmail.com>
2020-11-08 10:45:41 -0800

    Final commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,31 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Diagnostics;
+using System.IO;
 
 namespace DayZCleaner {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
+    /*
+     * Copyright (c) 2020 Ryan Broman
+     */
     public partial class MainWindow : Window {
-        public MainWindow()
-        {
+        public MainWindow() {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Console.WriteLine("Button is pressed!!");
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            Process[] collection = Process.GetProcesses();
+            foreach(var p in from Process p in collection
+                             where p.ProcessName.Contains("DayZ")
+                                   && p.ProcessName != Process.GetCurrentProcess().ProcessName
+                             select p) {
+                try {
+                    p.Kill();
+                } catch(Exception exc) {
+                    MessageBox.Show(exc.ToString(), "Error, please report this to Whitename!");
+                }
+            }
+            String sPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var dir = new DirectoryInfo(sPath + "\\DayZ");
+
+            foreach(var file in dir.EnumerateFiles("*.log")) {
+                file.Delete();
+            }
+
+            foreach(var file in dir.EnumerateFiles("*.adm")) {
+                file.Delete();
+            }
+
+            foreach(var file in dir.EnumerateFiles("*.rpt")) {
+                file.Delete();
+            }
+
+            foreach(var file in dir.EnumerateFiles("*.mdmp")) {
+                file.Delete();
+            }
+
+            MessageBox.Show("DayZ cleanup complete!", "Operation Complete");
         }
     }
 }
